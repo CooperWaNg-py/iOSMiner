b9a79c2d5de30358e6017bb7e06a9d7c3b27f7ba
CooperWaNg-py <cooper.wang.wtd@gmail.com>
2026-03-04 17:55:42 +1100

    Initial Commit

---
+++ iOSMinerTests/iOSMinerTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSMinerTests.swift
+//  iOSMinerTests
+//
+//  Created by Cooper Wang on 4/3/2026.
+//
+
+import Testing
+@testable import iOSMiner
+
+struct iOSMinerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
